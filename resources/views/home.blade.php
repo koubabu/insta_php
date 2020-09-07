eea1b12eb875ba6885d283caec1612e929618b87
koubabu <koubabu@gmail.com>
2020-09-07 14:31:29 +0530

    inital commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fccu10-1.fna.fbcdn.net/v/t51.2885-19/s150x150/106510147_2651008048479474_8371245779971824130_n.jpg?_nc_ht=instagram.fccu10-1.fna.fbcdn.net&_nc_ohc=RZfPoKWqYVUAX_QoX6R&oh=38a62e4a0995278a237688263be4ddce&oe=5F7E8411" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>KoushikMajhi</h1>
+            </div>
+            <div class="d-flex">
+                <div class="div pr-3"><strong>5</strong> posts</div>
+                <div class="div pr-3"><strong>152</strong> followers</div>
+                <div class="div pr-3"><strong>212</strong> following</div>
+            </div>
+            <div class="div pt-4 font-weight-bold   ">koubabu@gmail.com</div>
+            <div class="div">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elem</div>
+            <div class="div"><a href="#">website</a></div>
+        </div>
+
+    </div>
+    <div class="row pt-5">
+        <div class="col-4"><img src="https://instagram.fccu10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c136.0.808.808a/s640x640/118622846_785633882200514_2808365919015529054_n.jpg?_nc_ht=instagram.fccu10-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=mH84hJ1Zh64AX_LhPLP&oh=cce4ed9a1c5c7ad616c9bc72f4e8e059&oe=5F7EA85F" class="w-100"></div>
+        <div class="col-4"><img src="https://instagram.fccu10-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/106392201_3362897983786894_7946978454008291114_n.jpg?_nc_ht=instagram.fccu10-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=KHH7bj50Q9wAX9kNU1s&oh=3f757670ec69d5238f58616ec6306792&oe=5F810C91" class="w-100"></div>
+        <div class="col-4"><img src="https://instagram.fccu10-1.fna.fbcdn.net/v/t51.2885-15/e15/s240x240/106204497_1530287500464762_8877727478913263129_n.jpg?_nc_ht=instagram.fccu10-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=-5QIsCjK-MQAX_dNDDV&oh=330ad343d27220d2ccfbe705d3e0b42b&oe=5F7E873F" class="w-100"></div>
+    </div>
+</div>
+@endsection
